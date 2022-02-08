b46cd7067adb2089cc8f09172c05cbac4fc10eac
Rafael <rafael.jorge.silva.francisco@gmail.com>
2022-02-08 13:05:49 -0300

    Comentario JS

---
+++ Fundamentos/comentario.js
@@ -0,0 +1,11 @@
+// console.log é usado para exibir algo na tela do computador!
+console.log('Passo #01')
+console.log('Passo #03') // mais exemplo de comentário
+console.log('Passo #02')
+
+/* 
+    * Esse é um 
+    * um comentário de
+    * multiplas linhas!
+*/
+
