0ab2d0de62b4946537431c2775973a085422b18a
Rafael <rafael.jorge.silva.francisco@gmail.com>
2022-02-08 14:43:47 -0300

    desafio troca JS

---
+++ Fundamentos/desafioTroca.js
@@ -0,0 +1,13 @@
+let a = 7;
+let b = 94;
+
+console.log("valor de a: " + a + ", valor de b: " + b);
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log("valor de a: " + a + ", valor de b: " + b);
+
+
+
